d5c32f23787c865b9f1b5c59dde5b2127dbce2b9
greatfiredragon <kulmike2002@gmail.com>
2023-12-09 18:35:13 +0300

    Вопрос с загрузкой решен(?)!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -44,11 +44,13 @@
 </script>
 
 {#if isLoading}
-  <BlobBG />
-  <main class="fixed top-0 left-0 z-[100] flex items-center justify-center w-screen h-screen">
+  <main
+    class="fixed top-0 left-0 z-[100] bg-neutral flex items-center justify-center w-screen h-screen"
+  >
     <Loader />
   </main>
 {/if}
+<BlobBG />
 
 <span
   transition:slide
